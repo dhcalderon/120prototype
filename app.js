3005d710f5eada1b210ca34075b2e40f8556402e
dhcalderon <dhcalder@ucsd.edu>
2021-03-05 08:03:16 -0800

    Trying something out

--- app.js
+++ app.js
@@ -48,6 +48,6 @@ app.get('/deleted', get.delete);
 // Example route
 // app.get('/users', user.list);
 
-app.listen(app.get('port'), function(){
+http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
 });
